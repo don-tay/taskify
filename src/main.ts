6e390e1d61249372a4e02c705287d8cb44a38891
Don Tay Xi Cheng <dontay0209@gmail.com>
2021-05-31 16:53:16 +0800

    Add swagger to project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder().setTitle('Taskify API').setDescription('Taskify API endpoints').build();
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDoc);
+
   await app.listen(3000);
 }
 bootstrap();
